4851034e3b8bf31b873dffabec5b60a048feb39c
Allah Wasaya <136077480+wasaya33@users.noreply.github.com>
2023-09-13 10:15:39 +0500

    Add files via upload

---
+++ script/logic.js
@@ -0,0 +1,54 @@
+const inputBox = document.getElementById("inputBox");
+const listContainer = document.getElementById("listContainer");
+
+
+inputBox.addEventListener("keypress",(e)=>{
+    if(e.key=="Enter"){
+        addTask()
+    }
+})
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something");
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    saveData();
+}
+listContainer.addEventListener("click" , function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+            e.target.parentElement.remove();
+            saveData();
+        }
+    
+},false);
+
+
+function saveData(){
+    localStorage.setItem("data" , listContainer.innerHTML);
+}
+
+
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
+
+
+
+
+
+
+
+
